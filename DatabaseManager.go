82ad7fbb3885358f30fc9e790307937d77f97eb5
TomSavas <tomas1savickas@gmail.com>
2017-02-19 21:28:59 +0200

    Fix array intersection, id counter

--- DatabaseManager.go
+++ DatabaseManager.go
@@ -63,17 +63,17 @@ func GetTodoByPriorities(priorities []string) []Todo {
 	if len(priorities) == 0 {
 		query = "SELECT * FROM todos"
 	} else {
-		query = "SELECT * FROM todos WHERE Priority LIKE \"" + strings.Join(priorities, "\" OR Priority LIKE \"") + "\""
+		query = "SELECT * FROM todos WHERE Priority LIKE \"%" + strings.Join(priorities, "%\" OR Priority LIKE \"%") + "%\""
 	}
 	return GetTodosFromQuery(query)
 }
 
-func GetTodoByStatus(statuses []string) []Todo {
+func GetTodoByStatuses(statuses []string) []Todo {
 	var query string
 	if len(statuses) == 0 {
 		query = "SELECT * FROM todos"
 	} else {
-		query = "SELECT * FROM todos WHERE Status LIKE \"" + strings.Join(statuses, "\" OR Status LIKE \"") + "\""
+		query = "SELECT * FROM todos WHERE Status LIKE \"%" + strings.Join(statuses, "%\" OR Status LIKE \"%") + "%\""
 	}
 	return GetTodosFromQuery(query)
 }
@@ -83,7 +83,7 @@ func GetTodoByTypes(types []string) []Todo {
 	if len(types) == 0 {
 		query = "SELECT * FROM todos"
 	} else {
-		query = "SELECT * FROM todos WHERE Type LIKE \"" + strings.Join(types, "\" OR Type LIKE \"") + "\""
+		query = "SELECT * FROM todos WHERE Type LIKE \"%" + strings.Join(types, "%\" OR Type LIKE \"%") + "%\""
 	}
 	return GetTodosFromQuery(query)
 }
@@ -103,16 +103,14 @@ func GetTodosFromQuery(query string) []Todo {
 	for s, err := db.Query(query); err == nil; err = s.Next() {
 		var rowid int64
 		s.Scan(&rowid, row)
-		fmt.Println(rowid, row)
-		todos = append(todos, Todo{rowid, CastToString(row["Task"]), CastToString(row["Priority"]), CastToString(row["Status"]), CastToString(row["Type"]), CastToString(row["Notes"])})
+		todos = append(todos, Todo{int(rowid), CastToString(row["Task"]), CastToString(row["Priority"]), CastToString(row["Status"]), CastToString(row["Type"]), CastToString(row["Notes"])})
 	}
 
 	return todos
 }
 
 func GetTodos(priorities, statuses, types []string) []Todo {
-	//TODO: fix this, totally doesn't work
-	Min := func (arrays [][]Todo, nums ...int64) int {
+	Min := func (arrays [][]Todo, nums []int) int {
 		min := 0
 		minValue := arrays[0][nums[0]]
 		for i := 1; i < len(nums); i++ {
@@ -124,7 +122,7 @@ func GetTodos(priorities, statuses, types []string) []Todo {
 		return min
 	}	
 
-	Equality := func (arrays [][]Todo, nums ...int64) bool{
+	Equality := func (arrays [][]Todo, nums []int) bool{
 		for i := 1; i < len(nums); i++ {
 			if !arrays[i-1][nums[i-1]].Equals(arrays[i][nums[i]]){
 				return false
@@ -133,31 +131,62 @@ func GetTodos(priorities, statuses, types []string) []Todo {
 		return true
 	}
 
+	CheckIfDone := func (arrays [][]Todo, indexes []int) bool {
+		for index, value := range indexes {
+			if int(value) != len(arrays[index]) - 1 {
+				return false
+			} 
+		}
+		return true
+	}
+
 	IntersectArrays := func (arrays ...[]Todo) []Todo {
 		var todos []Todo
+		var indexes []int
 
-		var indexes [len(arrays)]int64
-		for index, _ := range arrays{
-			indexes[index] = 0
+		for _, todos := range arrays {
+			if cap(todos) == 0 {
+				return []Todo{}
+			}
+		}
+			
+		for _, _ = range arrays{
+			indexes = append(indexes, 0)
 		} 
 
-		for {
+		for i := 0; ; i++ {
 			if Equality(arrays, indexes){
-				todos.append(arrays[0][indexes[0]])
+				todos = append(todos, arrays[0][indexes[0]])
 			}
 
-			min, minTodo := Min(arrays, indexes)
-			if(min != len(arrays[min])-1){
+			if CheckIfDone(arrays, indexes) {
+				break
+			}
+
+			min := Min(arrays, indexes)
+			if(indexes[min] < len(arrays[min]) - 1){
 				indexes[min]++	
+			} else {
+				break
 			}
 		}
 		return todos
 	}
-	
-	return IntersectArrays(GetTodoByPriorities(priorities), GetTodoByStatus(statuses), GetTodoByTypes(types))
+
+	return IntersectArrays(GetTodoByPriorities(priorities), GetTodoByStatuses(statuses), GetTodoByTypes(types))
 }
 
 func AddTodo(todo Todo) {
-	err := db.Exec(fmt.Sprintf("INSERT INTO todos VALUES(%v, \"%v\", \"%v\", \"%v\", \"%v\", \"%v\")", todo.Id, todo.Task, todo.Priority, todo.Status, todo.Type, todo.Notes))
-	fmt.Println(err)
+	db.Exec(fmt.Sprintf("INSERT INTO todos VALUES(%v, \"%v\", \"%v\", \"%v\", \"%v\", \"%v\")", GetLastID() + 1, todo.Task, todo.Priority, todo.Status, todo.Type, todo.Notes))
+}
+
+func GetLastID() int {
+	response, err := db.Query("SELECT * FROM todos WHERE ID = (SELECT MAX(ID) FROM todos);")
+	if err != nil {
+		return 0
+	}
+
+	var rowid int64
+	response.Scan(&rowid, make(sqlite3.RowMap))
+	return int(rowid)
 }
